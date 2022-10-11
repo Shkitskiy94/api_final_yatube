474a3abdfddd96553e3cd154c6bfbddfe5b9312b
Yuriy Shkitskiy <shkitskiy94@gmail.com>
2022-10-11 15:05:08 +0300

    REST API v 1.0

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from posts.models import Follow, Group, Post, User
-from rest_framework import filters, mixins, permissions, viewsets
+from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from .customviewset import CreateRetrieveViewSet
