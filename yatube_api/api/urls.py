b04bfac3ae8082a24123adeab88d43fb0ac66601
Yuriy Shkitskiy <shkitskiy94@gmail.com>
2022-10-12 23:41:57 +0300

    REST API v 1.1 после ревью

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenObtainPairView,\
+                                           TokenRefreshView,\
+                                           TokenVerifyView
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
@@ -14,7 +16,11 @@ v1_router.register(r'groups', GroupViewSet, basename='group')
 v1_router.register(r'follow', FollowViewSet, basename='follow')
 
 jwt_patterns = [
-    path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'jwt/create/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
