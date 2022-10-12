cfe1f696b47c1248792d9d7319f32cb02a776a29
Yuriy Shkitskiy <shkitskiy94@gmail.com>
2022-10-12 23:45:10 +0300

    REST API v 1.1 после ревью

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView,\
-                                           TokenRefreshView,\
-                                           TokenVerifyView
+    TokenRefreshView,\
+    TokenVerifyView
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
